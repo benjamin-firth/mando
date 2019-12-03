764399310b09112d7c053e51f9357aa726edcfea
Ben Firth <bfirth93@gmail.com>
2019-12-03 14:15:57 -0700

    Create initial movie page

---
+++ src/components/MoviePage/MoviePage.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const MoviePage = () => {
+  return (
+    <section className="main">
+      Waddup - Here's the container for movies
+      <article className="movie">
+        <div>
+          <h2>Title</h2>
+          <p>Episode #</p>
+          <p>Release Year</p>
+          <button type="button">View Characters</button>
+        </div>
+        <img src={logo} className="movie-picture" alt="Movie Poster" />
+      </article>
+      <article className="movie">
+        <div>
+          <h2>Title</h2>
+          <p>Episode #</p>
+          <p>Release Year</p>
+          <button type="button">View Characters</button>
+        </div>
+        <img src={logo} className="movie-picture" alt="Movie Poster" />
+      </article>
+    </section>
+  )
+}
+
+export default MoviePage;
