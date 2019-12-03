290bc0c5068247529c3064c84b360ae97d48cb22
Ben Firth <bfirth93@gmail.com>
2019-12-03 15:43:23 -0700

    Change app to class component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from '../../logo.svg';
 import './App.scss';
 import WelcomeForm from '../WelcomeForm/WelcomeForm.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>
-          MANDO
-        </h1>
-      </header>
-      <WelcomeForm />
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      currentPage: 'Landing',
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>
+            MANDO
+          </h1>
+        </header>
+        <WelcomeForm />
+      </div>
+    );
+  }
 }
 
 export default App;
