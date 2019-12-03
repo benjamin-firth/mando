0f9a2a8ba2718fcad67b6a8c2ce1e15b59590314
Ben Firth <bfirth93@gmail.com>
2019-12-03 16:09:54 -0700

    Add change page to movie page functionality

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
-import logo from '../../logo.svg';
 import './App.scss';
-import WelcomeForm from '../WelcomeForm/WelcomeForm.js'
+import WelcomeForm from '../WelcomeForm/WelcomeForm.js';
+import MoviePage from '../MoviePage/MoviePage.js';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      currentPage: 'Landing',
+      currentPage: 'WelcomeForm',
     };
   }
-  render() {
+
+  changePage = (page) => {
+    this.setState({currentPage: page});
+    console.log(this.state);
+  }
+
+  renderLandingPage = () => {
     return (
       <div className="App">
         <header className="App-header">
@@ -18,9 +24,31 @@ class App extends Component {
             MANDO
           </h1>
         </header>
-        <WelcomeForm />
+        <WelcomeForm changePage={this.changePage}/>
       </div>
     );
+  }
+
+  renderMoviePage = () => {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>
+            MANDO
+          </h1>
+        </header>
+        <MoviePage changePage={this.changePage}/>
+      </div>
+    );
+  }
+
+  render(name) {
+    const { currentPage } = this.state;
+    if (currentPage === 'WelcomeForm') {
+      return this.renderLandingPage();
+    } else if (currentPage === 'MoviePage') {
+      return this.renderMoviePage();
+    }
   }
 }
 
--- src/components/MoviePage/MoviePage.js
+++ src/components/MoviePage/MoviePage.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import logo from '../../logo.svg';
+
 
 const MoviePage = () => {
   return (
