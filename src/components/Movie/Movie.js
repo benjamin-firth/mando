bf3a1555b4111bb1211f7e37060fd919287410e2
Ben Firth <bfirth93@gmail.com>
2019-12-05 15:46:17 -0700

    Add episode to button on click

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const Movie = ({ title, episode, release, opening, image }) => {
+const Movie = ({ title, episode, release, opening, image, filterMovie }) => {
   return (
     <article className="movie">
       <div>
         <h2>{title}</h2>
         <p>Episode #{episode}</p>
         <p>Release Year: {release}</p>
-        <button type="button">View Characters</button>
+        <button id={episode} type="button" onClick={(e) => filterMovie(e.target.id)}>View Characters</button>
       </div>
       <img src={image} className="movie-picture" alt="Movie Poster" />
     </article>
