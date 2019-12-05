d8c6fb2f64f1865efa11ba54b9d45e7575f9d0e1
Ben Firth <bfirth93@gmail.com>
2019-12-05 08:51:41 -0700

    Add user profile component skeleton

--- src/components/UserProfile/UserProfile.js
+++ src/components/UserProfile/UserProfile.js
@@ -1,13 +1,13 @@
-// import React from 'react';
+import React from 'react';
 
-// const UserProfile = () => {
-//   return (
-//     <nav>
+const UserProfile = ({ currentUser }) => {
+  return (
+    <nav>
+      <h3>{currentUser.currentName}</h3>
+      <p>{currentUser.currentQuote}</p>
+      <h4>{currentUser.currentRank}</h4>
+    </nav>
+  )
+}
 
-//     </nav>
-//   )
-
-
-// }
-
-// export default UserProfile;
+export default UserProfile;
