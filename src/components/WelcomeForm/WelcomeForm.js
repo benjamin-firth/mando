0786caaee5f2aadbe079bbfd139d5a960f615c04
Ben Firth <bfirth93@gmail.com>
2019-12-05 22:20:10 -0700

    Add transparent baby yoda

--- src/components/WelcomeForm/WelcomeForm.js
+++ src/components/WelcomeForm/WelcomeForm.js
@@ -55,7 +55,7 @@ class WelcomeForm extends Component {
         </select>
         <button onClick={this.errorCheck} className='login-button' >Enter, you will!</button>
         <p className={this.state.errorMsg}>Enter all forms correctly, you must!</p>
-        <img alt='Yoda shows up when login is invalid' src='https://1.bp.blogspot.com/-2zleuWnMiP4/XdmjP70dUPI/AAAAAAAAAZw/ZXK_NaTLn-cvCP7VIEHxNsA7zcaqEt6nACLcBGAsYHQ/s1600/baby-yoda-by-leo-batic.jpg' className={this.state.errorMsg} />
+        <img alt='Yoda shows up when login is invalid' src='https://66.media.tumblr.com/a3b90eadcb7a53a60a56a73b1d28f5b1/b65a5b0b8c743787-e7/s400x600/bd438f1eea7c1e7593de491f7c7f50f986fe4257.png' className={this.state.errorMsg} />
       </form>
     )
   }
