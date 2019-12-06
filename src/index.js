fd3a3be5a0b88607eeeed95f4f2f8ccc7cd84e5d
Ben Firth <bfirth93@gmail.com>
2019-12-05 22:15:12 -0700

    Add router dependency code

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './components/App/App.js';
 import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+import { BrowserRouter } from 'react-router-dom';
+const router = (
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+)
+ReactDOM.render(router, document.getElementById('root'));
 serviceWorker.unregister();
