91fc3c4e197ef4724c20fa6c95ad23c15d76a645
Ben Firth <bfirth93@gmail.com>
2019-12-07 16:06:43 -0700

    Add handleChange test for state change

--- src/components/WelcomeForm/WelcomeForm.js
+++ src/components/WelcomeForm/WelcomeForm.js
@@ -19,7 +19,7 @@ class WelcomeForm extends Component {
   }
 
   errorCheck = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     const { currentName, currentQuote, currentRank } = this.state;
     if (currentName === '' || currentQuote === '') {
       this.setState({errorMsg: 'visibleError'});
@@ -58,7 +58,7 @@ class WelcomeForm extends Component {
           <option value='jediknight'>Jedi Knight</option>
           <option value='jedimaster'>Jedi Master</option>
         </select>
-        <button onClick={this.errorCheck} className='login-button' >Enter, you will!</button>
+        <button onClick={this.errorCheck} type='button' className='login-button' >Enter, you will!</button>
         <p className={this.state.errorMsg}>Enter all forms correctly, you must!</p>
         <img alt='Yoda shows up when login is invalid' src='https://66.media.tumblr.com/a3b90eadcb7a53a60a56a73b1d28f5b1/b65a5b0b8c743787-e7/s400x600/bd438f1eea7c1e7593de491f7c7f50f986fe4257.png' className={this.state.errorMsg} />
       </form>
--- src/components/WelcomeForm/WelcomeForm.test.js
+++ src/components/WelcomeForm/WelcomeForm.test.js
@@ -5,13 +5,45 @@ import WelcomeForm from './WelcomeForm.js'
 describe('Welcome Form', () => {
   let wrapper;
   let mockChangePage;
+  let mockErrorCheck;
   
   beforeEach(() => {
     mockChangePage = jest.fn();
+    mockErrorCheck = jest.fn();
     wrapper = shallow(<WelcomeForm changePage={mockChangePage}/>)
   })
 
   it('should match the snapshot', () => {
     expect(wrapper).toMatchSnapshot();
   })
+
+  it('should update state when handleChange is called', () => {
+    const mockState = {
+      currentName: '',
+      currentQuote: '',
+      currentRank: 'padawan',
+      errorMsg: 'invisibleError'
+    };
+    const expected = {
+      currentName: '',
+      currentQuote: '',
+      currentRank: 'Sith Lord',
+      errorMsg: 'invisibleError'
+    }
+    wrapper.setState(mockState);
+    wrapper.instance().handleChange('currentRank', 'Sith Lord');
+
+    expect(wrapper.state()).toEqual(expected);
+  })
+
+  it('should call the errorCheck function when the button is clicked', () => {
+
+    const mockEvent = { preventDefault: jest.fn() };
+    wrapper.instance().mockErrorCheck = jest.fn();
+
+    wrapper.find('button').simulate('click', mockEvent);
+    expect(wrapper.instance().mockErrorCheck).toHaveBeenCalled();
+  })
+
+
 })
