d01ecfc986d2e24651d1058f3bcb1eaa40949343
Nick Nist <nicknistoc@gmail.com>
2019-12-09 15:35:53 -0700

    Add UserProfile tests to reach 100%

--- src/components/UserProfile/UserProfile.js
+++ src/components/UserProfile/UserProfile.js
@@ -2,15 +2,13 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const UserProfile = ({ currentUser, changePage, handleImgClick }) => {
-  console.log(currentUser.name);
-
   return (
     <section className='profile-box'>
       <h3>Name: {currentUser.name}</h3>
       <p>"{currentUser.quote}"</p>
       <h4>Experience Level: {currentUser.rank}</h4>
       <NavLink id='welcome-form' onClick={() => changePage('WelcomeForm', {}, 'hide')} exact to={'/'}>Log Out</NavLink>
-      <NavLink exact to={'/movie-page'} onClick={() => handleImgClick()}>Home</NavLink>
+      <NavLink id='nav-link2' exact to={'/movie-page'} onClick={() => handleImgClick()}>Home</NavLink>
     </section>
   )
 }
--- src/components/UserProfile/UserProfile.test.js
+++ src/components/UserProfile/UserProfile.test.js
@@ -5,7 +5,8 @@ import UserProfile from './UserProfile.js'
 describe('UserProfile', () => {
   let wrapper;
   let mockProps;
-
+  let mockChangePage;
+  let mockHandleImgClick;
 
   beforeEach(() => {
     mockProps = {
@@ -13,10 +14,24 @@ describe('UserProfile', () => {
       quote: 'You send them to me!',
       rank: 'Master'
     }
-    wrapper = shallow(<UserProfile currentUser={mockProps}/>)
+    mockChangePage = jest.fn();
+    mockHandleImgClick = jest.fn();
+    wrapper = shallow(<UserProfile currentUser={mockProps} changePage={mockChangePage} handleImgClick={mockHandleImgClick}/>)
   })
 
   it('should match the snapshot', () => {
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should fire changePage when navLink is clicked', () => {
+    wrapper.find('#welcome-form').simulate('click');
+
+    expect(mockChangePage).toHaveBeenCalledWith('WelcomeForm', {}, 'hide');
+  });
+
+  it('should fire handleImgClick when other navLink is clicked', () => {
+    wrapper.find('#nav-link2').simulate('click');
+
+    expect(mockHandleImgClick).toHaveBeenCalled();
   })
 })
